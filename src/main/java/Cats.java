84200a6081a20bfadb4804d25daa1fe311620667
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2022-09-02 20:38:01 +0700

    Cats

---
+++ src/main/java/Cats.java
@@ -0,0 +1,31 @@
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Cats {
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final Integer upvote;
+
+    public Cats(@JsonProperty("id") String id, @JsonProperty("text") String text, @JsonProperty("type") String type,
+                @JsonProperty("user") String user, @JsonProperty("upvotes") Integer upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvote = upvotes;
+    }
+
+    public Integer getUpvote() {
+        return upvote;
+    }
+    @Override
+    public String toString() {
+        return "\n id " + id +
+                "\n text " + text +
+                "\n type " + type +
+                "\n user " + user +
+                "\n upvote " + upvote;
+    }
+}
